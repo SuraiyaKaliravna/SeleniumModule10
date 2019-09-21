590152fdd4af799a84d5aae94d9b84982912b585
SuraiyaKaliravna <55441408+SuraiyaKaliravna@users.noreply.github.com>
2019-09-21 20:56:22 +0100

    Added comments
    //--------------------------Methode to click on Enroll Button--------------------

--- FrameworkModule10/src/pages/SelectCourse.java
+++ FrameworkModule10/src/pages/SelectCourse.java
@@ -7,7 +7,7 @@ public class SelectCourse {
 	
 	ActionKeyWords AK = new ActionKeyWords();
 	LocatorVariable LV = new LocatorVariable();
-	
+//--------------------------Methode to click on Enroll Button--------------------	
 	public void clickEnrollCourse() throws Exception {
 		AK.scrolling();
 		AK.explicitWait(LV.EnrollCourseLocValue_SelectCourse);
